48e355203062fd252bf1a2a6beeda90676d9c766
prazd <alexokda@list.ru>
2018-09-10 01:26:07 +0300

    bot/scenes

--- bot.js
+++ bot.js
@@ -3,28 +3,28 @@ const Markup = require("telegraf/markup");
 const Stage = require("telegraf/stage");
 const session = require("telegraf/session");
 const WizardScene = require("telegraf/scenes/wizard");
-const Scene = require("telegraf/scenes/base")
 abi = require("./contractData").CONTRACT_ABI,
 contractAddress = require("./contractData").CONTRACT_ADDRESS
 const Web3 = require('web3')
-const bot = new Telegraf('698889455:AAFs9dV1_yv95MP87-o5bmck_y4KKKoZ30w')
+const bot = new Telegraf(process.env('token'))
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 const instance = new web3.eth.Contract(abi, contractAddress);
 
 const menuKeyboard = Markup.inlineKeyboard([
     Markup.callbackButton("Add🚘","add"),
     Markup.callbackButton("Balance💰","balance"),
-    // Markup.callbackButton("Buy car🚘💰","buy")
+    Markup.callbackButton("Buy🚘💰","buy"),
   ]).extra()
 
 bot.start(ctx => {
     instance.methods.carDealer().call({from:web3.eth.accounts[2]},(err,doc)=>{
         if(err) throw err;
-        ctx.reply("Hello!!\nDealer address: "+doc, menuKeyboard)
+        ctx.reply("Hello!!\nDealer address: " + doc, menuKeyboard)
 })})
 
+
 const addCar = new WizardScene(
-    "add",
+    "addCar",
     ctx => {
       ctx.reply("The default function is Add a car\nPlease Enter a car price");
       return ctx.wizard.next();
@@ -128,16 +128,73 @@ bot.action("NO",(ctx)=>{
         ctx.reply("Okay)TryAgain!!!\nDealer address: "+doc, menuKeyboard)})
 })
 
+
+
+const buyCar = new WizardScene(
+    "buyCar", ctx=> {
+    ctx.reply("Car ID:")
+    return ctx.wizard.next()
+},
+    (ctx)=>{
+        ctx.session.id = ctx.message.text
+        ctx.reply("Price:")
+        return ctx.wizard.next()
+    },
+    ctx =>{
+        ctx.session.value = ctx.message.text
+        ctx.reply("Enter your public key(hex): ")
+    return ctx.wizard.next()    
+    },  ctx =>{
+        ctx.session.key = ctx.message.text
+        ctx.reply("Enter gas Price:")
+    return ctx.wizard.next()    
+    },
+    ctx =>{
+        ctx.session.gasPrice = ctx.message.text
+        ctx.reply("Check this info:\n1.CarID: " + ctx.session.id + "\n"
+    +"2.Value:" + ctx.session.value + "\n"+"3.Public key(hex): " + ctx.session.key +"\n"+
+     "4.Gas Price: " + ctx.session.gasPrice)
+
+    return ctx.wizard.next()    
+    },
+    ctx=>{
+        if(ctx.message.text=="YES"){
+
+                instance.methods.buyCar(Number(ctx.session.id)).send({from:ctx.session.key, value: Number(ctx.session.value), gas: 9999999999, gasPrice: ctx.session.gasPrice})
+                .on('transactionHash', function(hash){
+                    ctx.reply(`TX Hash: ${hash}`);
+                })
+                .on('receipt', function(receipt){
+                    ctx.reply(`Car ID: ${receipt.events.BuyCar.returnValues.carID}`);
+                    ctx.reply(`Car owner ID: ${receipt.events.BuyCar.returnValues.carOwnerID}`);
+                    if (receipt.events.NewCarOwner) {
+                        ctx.reply(`Car owner address: ${receipt.events.NewCarOwner.returnValues.carOwnerAddress}`);
+                    }
+                    ctx.reply("Nice trade)",menuKeyboard)
+                })
+                .on('error', console.error);
+        }else if(ctx.message.text=="NO"){
+            ctx.reply(
+                "Okay)\n", menuKeyboard
+        )
+        }
+    ctx.scene.leave()
+    }
+)
+
 bot.action("balance",(ctx)=>{
     instance.methods.carDealer().call({from:web3.eth.accounts[2]},(err,doc)=>{
         if(err) throw err;
         web3.eth.getBalance(doc).then(balance=>{
-            ctx.reply("Dealer address: "+ doc +"\nBalance: "+ balance, menuKeyboard )})
+            ctx.editMessageText("Dealer address: "+ doc +"\nBalance: "+ balance, menuKeyboard)})
         })
 })
 
-
-const stage = new Stage([addCar], {default:"add"});
+const stage = new Stage();
+stage.register(buyCar)
+stage.register(addCar)
 bot.use(session());
 bot.use(stage.middleware());
+bot.action("buy",ctx=>{ctx.scene.enter("buyCar")})
+bot.action("add",ctx=>{ctx.scene.enter("addCar")})
 bot.startPolling(); 
